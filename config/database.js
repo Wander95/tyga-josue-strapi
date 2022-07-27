84e6e1b0e9cc16b577737d8a11a7814c84945005
Wander Sanchez <wander.sanchez@outlook.com>
2022-07-27 19:17:08 -0400

    🎉 Project initial config

---
+++ config/database.js
@@ -0,0 +1,20 @@
+module.exports = ({ env }) => {
+  return ({
+    connection: {
+      client: 'postgres',
+      connection: {
+        host: env('DATABASE_HOST', process.env.DATABASE_HOST),
+        port: env.int('DATABASE_PORT',process.env.DATABASE_PORT),
+        database: env('DATABASE_NAME',process.env.DATABASE_NAME),
+        user: env('DATABASE_USERNAME',process.env.DATABASE_USERNAME),
+        password: env('DATABASE_PASSWORD',process.env.DATABASE_PASSWORD),
+        ssl: env.bool('DATABASE_SSL', true),
+      },
+      pool: {
+        min: 0,
+        max: 3
+      }
+    },
+  });
+  
+}
